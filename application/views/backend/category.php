920634e8fe34610bfb3ace3c4f218184c3726a06
Alexandre Palma Gonçalves <alepalmag85ti@gmail.com>
2020-10-09 04:41:48 -0300

    Development of the category.php file (views> backend). Making the contents of the home.php and category.php (views> backend) files dynamic.

---
+++ application/views/backend/category.php
@@ -0,0 +1,57 @@
+<div id="page-wrapper">
+    <div class="row">
+        <div class="col-lg-12">
+            <h1 class="page-header"><?php echo $subtitle ?></h1>
+        </div>
+        <!-- /.col-lg-12 -->
+    </div>
+    <!-- /.row -->
+    <div class="row">
+        <div class="col-lg-12">
+            <div class="panel panel-default">
+                <div class="panel-heading">
+                    <?php echo $subtitle ?>
+                </div>
+                <div class="panel-body">
+                    <div class="row">
+                        <div class="col-lg-12">
+                            <form role="form">
+                                <div class="form-group">
+                                    <label>Title</label>
+                                    <input class="form-control" placeholder="Entre com o texto">
+                                </div>
+                                <div class="form-group">
+                                    <label>Featured Photo</label>
+                                    <input type="file">
+                                </div>
+                                <div class="form-group">
+                                    <label>Content</label>
+                                    <textarea class="form-control" rows="3"></textarea>
+                                </div>
+
+                                <div class="form-group">
+                                    <label>Selects</label>
+                                    <select class="form-control">
+                                        <option>1</option>
+                                        <option>2</option>
+                                        <option>3</option>
+                                        <option>4</option>
+                                        <option>5</option>
+                                    </select>
+                                </div>
+                                <button type="submit" class="btn btn-default">Register</button>
+                                <button type="reset" class="btn btn-default">Clear</button>
+                            </form>
+                        </div>
+
+                    </div>
+                    <!-- /.row (nested) -->
+                </div>
+                <!-- /.panel-body -->
+            </div>
+            <!-- /.panel -->
+        </div>
+        <!-- /.col-lg-12 -->
+    </div>
+    <!-- /.row -->
+</div>
--- application/views/backend/home.php
+++ application/views/backend/home.php
@@ -1,7 +1,7 @@
 <div id="page-wrapper">
     <div class="row">
         <div class="col-lg-12">
-            <h1 class="page-header">Page Title</h1>
+            <h1 class="page-header"><?php echo $subtitle ?></h1>
         </div>
         <!-- /.col-lg-12 -->
     </div>
@@ -10,40 +10,13 @@
         <div class="col-lg-12">
             <div class="panel panel-default">
                 <div class="panel-heading">
-                    Page Title
+                    <?php echo $subtitle ?>
                 </div>
                 <div class="panel-body">
                     <div class="row">
                         <div class="col-lg-12">
-                            <form role="form">
-                                <div class="form-group">
-                                    <label>Title</label>
-                                    <input class="form-control" placeholder="Entre com o texto">
-                                </div>
-                                <div class="form-group">
-                                    <label>Featured Photo</label>
-                                    <input type="file">
-                                </div>
-                                <div class="form-group">
-                                    <label>Content</label>
-                                    <textarea class="form-control" rows="3"></textarea>
-                                </div>
-
-                                <div class="form-group">
-                                    <label>Selects</label>
-                                    <select class="form-control">
-                                        <option>1</option>
-                                        <option>2</option>
-                                        <option>3</option>
-                                        <option>4</option>
-                                        <option>5</option>
-                                    </select>
-                                </div>
-                                <button type="submit" class="btn btn-default">Register</button>
-                                <button type="reset" class="btn btn-default">Clear</button>
-                            </form>
+                            <h2>Welcome to the administrative page of your blog!!!</h2>
                         </div>
-
                     </div>
                     <!-- /.row (nested) -->
                 </div>
