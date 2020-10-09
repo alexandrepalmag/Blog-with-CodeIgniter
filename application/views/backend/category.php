04962987dc79c086d696d5c08dd422109d262d02
Alexandre Palma Gonçalves <alepalmag85ti@gmail.com>
2020-10-09 06:06:54 -0300

    Development of dynamically inserting items with php in the category.php file (application / views / backend / category.php)

--- application/views/backend/category.php
+++ application/views/backend/category.php
@@ -1,21 +1,55 @@
 <div id="page-wrapper">
     <div class="row">
         <div class="col-lg-12">
-            <h1 class="page-header"><?php echo $subtitle ?></h1>
+            <h1 class="page-header"><?php echo 'Manage '.$subtitle ?></h1>
         </div>
         <!-- /.col-lg-12 -->
     </div>
     <!-- /.row -->
     <div class="row">
-        <div class="col-lg-12">
+        <div class="col-lg-6">
             <div class="panel panel-default">
                 <div class="panel-heading">
-                    <?php echo $subtitle ?>
+                    <strong><?php echo 'Add New ' . $subtitle ?></strong>
                 </div>
                 <div class="panel-body">
                     <div class="row">
                         <div class="col-lg-12">
-                            <form role="form">
+
+                        </div>
+
+                    </div>
+                    <!-- /.row (nested) -->
+                </div>
+                <!-- /.panel-body -->
+            </div>
+            <!-- /.panel -->
+        </div>
+        <!-- /.col-lg-6 -->
+        <div class="col-lg-6">
+            <div class="panel panel-default">
+                <div class="panel-heading">
+                    <strong><?php echo 'Change Existing ' . $subtitle ?></strong>
+                </div>
+                <div class="panel-body">
+                    <div class="row">
+                        <div class="col-lg-12">
+
+                        </div>
+
+                    </div>
+                    <!-- /.row (nested) -->
+                </div>
+                <!-- /.panel-body -->
+            </div>
+            <!-- /.panel -->
+        </div>
+        <!-- /.col-lg-6 -->
+    </div>
+    <!-- /.row -->
+</div>
+
+<!-- <form role="form">
                                 <div class="form-group">
                                     <label>Title</label>
                                     <input class="form-control" placeholder="Entre com o texto">
@@ -41,17 +75,4 @@
                                 </div>
                                 <button type="submit" class="btn btn-default">Register</button>
                                 <button type="reset" class="btn btn-default">Clear</button>
-                            </form>
-                        </div>
-
-                    </div>
-                    <!-- /.row (nested) -->
-                </div>
-                <!-- /.panel-body -->
-            </div>
-            <!-- /.panel -->
-        </div>
-        <!-- /.col-lg-12 -->
-    </div>
-    <!-- /.row -->
-</div>
+                            </form> -->
