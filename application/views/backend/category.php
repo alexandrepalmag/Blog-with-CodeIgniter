d6151473f5094376c4e810dec39a3bd4425b8ac0
Alexandre Palma Gonçalves <alepalmag85ti@gmail.com>
2020-10-09 06:53:55 -0300

    Development of the table in the category.php file

--- application/views/backend/category.php
+++ application/views/backend/category.php
@@ -34,9 +34,23 @@
                 <div class="panel-body">
                     <div class="row">
                         <div class="col-lg-12">
+                            <?php 
+                                $this->table->set_heading("Category Name", "Change","Delete");
+                                foreach($categories as $category) {
+                                    $getName=$category->title;
+                                    $change=anchor(base_url('admin/category'),'<a class="btn btn-info" aria-label="Delete"><i class="fa fa-pencil-square-o"></i></a>');
+                                    $delete=anchor(base_url('admin/category'),'<a class="btn btn-danger" aria-label="Delete"><i class="fa fa-trash fa-lg"></i></a>');
 
+                                    $this->table->add_row($getName,$change,$delete);
+
+                                }
+
+                                $this->table->set_template(array(
+                                    'table_open' => '<table class="table">'
+                                ));
+                                echo $this->table->generate();
+                            ?>
                         </div>
-
                     </div>
                     <!-- /.row (nested) -->
                 </div>
