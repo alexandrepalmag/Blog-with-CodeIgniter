9e82cecfb8cfdd7e158e40dc236fe4e606b082d1
Alexandre Palma Gonçalves <alepalmag85ti@gmail.com>
2020-10-09 06:19:20 -0300

    Added the loading of categories in the controller file Category.php and (application / controllers / admin / Category.php); loading the table building library.

--- application/controllers/admin/Category.php
+++ application/controllers/admin/Category.php
@@ -6,10 +6,15 @@ class Category extends CI_Controller{
     public function __construct()
     {
         parent::__construct();
+
+        $this->load->model('CategoriesModel', 'modelcategories');
+        $this-> categories = $this->modelcategories->listCategories();
     }
 
     public function index()
     {
+        $this->load->library('table');
+        $datas['categories'] = $this->categories;
         $datas['title'] = 'Control Panel';
         $datas['subtitle'] = 'Category';
         //end
