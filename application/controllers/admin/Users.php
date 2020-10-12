40bb056f703267b9393114281f7a2694c36bfd88
Alexandre Palma Gonçalves <alepalmag85ti@gmail.com>
2020-10-12 08:03:15 -0300

    creation loginPage() method in application/controllers/admin/Users.php file

---
+++ application/controllers/admin/Users.php
@@ -0,0 +1,31 @@
+<?php
+defined('BASEPATH') or exit('No direct script access allowed');
+
+class Home extends CI_Controller{
+
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    public function index()
+    {
+        $datas['title'] = 'Control Panel';
+        $datas['subtitle'] = 'Home';
+        //end
+        $this->load->view('backend/template/html-header', $datas);
+        $this->load->view('backend/template/template');
+        $this->load->view('backend/home');
+        $this->load->view('backend/template/html-footer');
+    }
+
+    public function loginPage() {
+        $datas['title'] = 'Control Panel';
+        $datas['subtitle'] = 'Login';
+        //end
+        $this->load->view('backend/template/html-header', $datas);
+        $this->load->view('backend/login');
+        $this->load->view('backend/template/html-footer');
+    }
+
+}
