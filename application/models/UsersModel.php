8978662cbee526b7fc4c4a4b301094455af8130e
Alexandre Palma Gonçalves <alepalmag85ti@gmail.com>
2020-10-16 07:56:05 -0300

    Inserting the Model loading

--- application/models/UsersModel.php
+++ application/models/UsersModel.php
@@ -1,7 +1,8 @@
 <?php
 defined('BASEPATH') or exit('No direct script access allowed');
 
-class UsersModel extends CI_Model{
+class UsersModel extends CI_Model
+{
 
     public $id;
     public $name;
@@ -16,18 +17,43 @@ class UsersModel extends CI_Model{
         parent::__construct();
     }
 
-    public function author_list($id) {
+    public function author_list($id)
+    {
         $this->db->select('id, name, historic, img');
         $this->db->from('user');
-        $this->db->where('id='.$id);
+        $this->db->where('id=' . $id);
         return $this->db->get()->result();
     }
 
-    public function list_authors() {
+    public function list_authors()
+    {
         $this->db->select('id, name, img');
         $this->db->from('user');
         $this->db->order_by('name', 'ASC');
         return $this->db->get()->result();
+    }
+
+    
+    /* Receive data that is sent from the "Category Name"
+form found in the category.php file */
+public function add($name,$email,$historic,$user,$password)
+{
+    $datas['name'] = $name;
+    $datas['email'] = $email;
+    $datas['historic'] = $historic;
+    $datas['user'] = $user;
+    $datas['password'] = md5($password);
+
+    return $this->db->insert('user', $datas);
+}
+
+/* method to delete a category in database
+    deleteCategory
+    Receive data that is sent from the "Category Name"
+    form found in the Category.php file*/
+    public function delete($id){
+        $this->db->where('md5(id)',$id);
+        return $this->db->delete('user');
     }
 
 }
